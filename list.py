5a68d8b4faca8a4d89e436ae0261498af8b08161
unknown <rawalhemant2059@gmail.com>
2024-05-13 21:06:02 +0545

    modiffied

---
+++ list.py
@@ -0,0 +1,66 @@
+'''
+syntax
+if (condition):
+
+  
+
+
+'''    
+# if (1==1):
+#     print("this is right")
+#     print("this is right")
+#     print("this is right")
+#     print("this is right")
+# else:
+#     print("this is wrong")
+
+# if(1==2):
+#     print("this is wrong")
+# elif(1==1):
+#     print("this is right")
+# else:
+#     print("both are wrong")
+
+# temp = 40
+# if(temp>40):
+#     print("hight temp")
+# elif(temp<40):
+#     print("low temp")
+# else:
+#     print("normal temp")
+# #debuger helps to check where the program goes..
+# #nested if
+# a = 10
+# b = 20
+# if (a == 10):
+#     print("this is 10")
+#     if (b > 30):
+#         print("the value is greater than be")
+#     else:
+#         print("this is wrong")
+# else:
+    # print("both is wrong")
+#afno grade ko division nikalne
+
+a= 3.25
+if(a >=0 and a<=4 ):
+    print("this is grade")
+    if(a>=0 and a<=1.6):
+        print("D+")
+    elif(a>=1.7 and a<=2.0):
+        print("C")
+    elif(a>=2.1 and a<2.4):
+        print("C+")
+    elif(a>=2.5 and a<=2.8):
+        print("B")
+    elif(a>=2.9 and a<=3.2):
+        print("B+")
+    elif(a>=3.3 and a<=3.6):
+        print("A")
+    elif(a>=3.7 and a<=4):
+        print("A+")
+    else:
+        print("you are fail")
+else:
+    print("input is wrong")
+
