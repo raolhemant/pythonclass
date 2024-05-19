ebab1bb357707abf0e00439d1420c319d233392d
unknown <rawalhemant2059@gmail.com>
2024-05-19 19:33:37 +0545

    initial

--- loop.py
+++ loop.py
@@ -1,2 +1,13 @@
 #for loop  finite number
 #while loop infinite number
+
+# for i in range(1,21):
+#     print(i)
+
+# a = ["ram","sam","rupesh"]
+# for i in a:
+#     print(i)
+
+n = int(input("ENTER THE VALUE "))
+for i in range (1,11):
+    print(f'{n} X {i} ={n*i}')
